487465fd6d6961798ea6d6bd416e53ff92dd9d41
jpca92 <jpca92@gmail.com>
2025-04-10 16:47:06 -0500

    lesson 1_4

--- api_edteam/urls.py
+++ api_edteam/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('courses/', views.course, name='course')
+    path('courses/', views.course, name='course'),
+    path('new_course/', views.post_course, name='post_course'),
+
     
 ]
--- api_edteam/views.py
+++ api_edteam/views.py
@@ -1,6 +1,9 @@
+import json
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from .models import Curso
+
 
 # Create your views here.
 
@@ -20,3 +23,31 @@ def course (request):
     }
     return JsonResponse(context)
 
+@csrf_exempt
+def post_course(request):
+    
+    json_data = json.loads(request.body)
+    
+    title = json_data['title']
+    description = json_data['description']
+    image = json_data['image']
+    
+    new_course = Curso.objects.create(
+        title=title,
+        description=description,
+        image=image
+    )
+    
+    new_dicc = {
+        'id':new_course.id,
+        'title':new_course.title,
+        'description':new_course.description,
+        'imagen':new_course.image
+    }
+    
+    context = {
+        'status':True,
+        'content':new_dicc
+    }
+    
+    return JsonResponse(context)
